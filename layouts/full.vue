2374e8f72e557ee55af572d38f0cf665c323e475
Luan van Pletsen <luanvanpletsen@gmail.com>
2026-02-10 14:27:36 +0000

    fix: add full layout with proper padding

---
+++ layouts/full.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="slidev-layout full px-14 py-10">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.full {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+</style>
