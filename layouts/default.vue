46333eb401dd992d93424ed3afb3bf582b48f6be
Luan van Pletsen <luanvanpletsen@gmail.com>
2026-02-10 12:39:50 +0000

    fix: add default layout with proper padding

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="slidev-layout default px-14 py-10">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.default {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+</style>
